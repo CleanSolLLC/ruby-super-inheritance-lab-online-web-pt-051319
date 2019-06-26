13949ab4a9ea702d91188be7b6bafcc5955c5054
Mark May <mamark90@gmail.com>
2019-06-26 22:11:49 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,6 +6,7 @@ class ChattyStudent < Student
   def hello
 
     super
+    binding.pry
       "How are you doing today? I'm okay, but I'm
       kind of tired. Did you watch The Walking Dead
       last night? You didn't?! Oh man, it was so crazy!
