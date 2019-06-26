9c6905bd085b169c7d1e53ae199d42a1a32f5264
Mark May <mamark90@gmail.com>
2019-06-26 22:11:39 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,8 +1,10 @@
+require 'pry'
 require_relative 'student.rb'
 
 class ChattyStudent < Student
 
   def hello
+    
     super
       "How are you doing today? I'm okay, but I'm
       kind of tired. Did you watch The Walking Dead
