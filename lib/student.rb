4965d9e913026998e3bc793c0dfdd3aa859e013c
Mark May <mamark90@gmail.com>
2019-06-26 22:44:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,11 @@
 class Student
+  attr_accessor :pick_me
 
   def hello
     puts "Hey there! I'm so excited to learn stuff."
   end
 
   def raise_hand
-    puts "Pick me!"
+    @pick_me = "Pick me!"
   end
 end
